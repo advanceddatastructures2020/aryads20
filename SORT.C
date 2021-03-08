1837ea235ba936df5711f910a20ad91e4ea0e9f4
aryasreerag <76676572+aryasreerag@users.noreply.github.com>
2021-03-08 12:27:50 +0530

    Add files via upload

---
+++ SORT.C
@@ -0,0 +1,30 @@
+#include<conio.h>
+#include<stdio.h>
+
+void main()
+{
+int i,j,a,n, number[30];
+clrscr();
+printf("enter the value of N \n");
+scanf("%d",&n);
+printf("enter the numbers \n");
+for(i=0;i<n;++i)
+scanf("%d",&number[i]);
+for(i=0;i<n;++i)
+{
+for(j=i+1;j<n;++j)
+{
+if (number[i]>number[j])
+{
+a=number[i];
+number[i]=number[j];
+number[j]=a;
+}
+}
+}
+printf("Sorted Array \n");
+for(i=0;i<n;++i)
+
+printf("%d\n",number[i]);
+getch();
+}
